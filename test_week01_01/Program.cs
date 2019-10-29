5f43e6107b7a6b7d014c7fb04821460741628e66
natumeyuzuru <LHH_github@users.noreply.github.com>
2019-10-29 22:47:11 +0800

    Add project files.

---
+++ test_week01_01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace test_week01_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");//consle_控制台
+            double x = 3, y = 5, res;
+             res= Max(x, y);
+            Console.WriteLine("max={0}", res);
+            //console.readkey();字面意思
+        }
+       public static double Max(double x,double y)
+        {
+            if (x > y)
+                return x;
+            else
+                return y;
+        }
+    }
+}
---
+++ week01_test02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace week01_test02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
